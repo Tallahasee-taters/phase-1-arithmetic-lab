c3306a473ec43fd5f945934a82e4627e6e9eb54f
Tate <thandley1234@gmail.com>
2022-11-26 18:36:30 -0700

    commit tree a281cd2820cc655a3ac254fe42cef1b65f176c1d

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = 62
+const random = Math.floor(Math.random() * 10)
+const num3 = 3
+const num4 = 1
+const mod = 4
+const max = 20
